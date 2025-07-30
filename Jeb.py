33ed16a0d2bb22837904381f1158d52d86f773e4
Nova <novagaming860@gmail.com>
2025-07-30 18:56:44 -0500

    Update Jeb.py

--- Jeb.py
+++ Jeb.py
@@ -1,8 +1,12 @@
-import webbrowser
-import time
-
-rickroll_url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-
-while True:
-    webbrowser.open(rickroll_url)
-    time.sleep(2)
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <title>Jeb!</title>
+</head>
+<body>
+  <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" rel="noopener noreferrer">
+    Click me for a surprise!
+  </a>
+</body>
+</html>
